1ee6274125cddbd120db7d56e919828344a6f8d8
Kota117 <dakotah1375@gmail.com>
2026-01-31 10:21:55 -0700

    Copy static dir into public dir

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
-from textnode import TextType, TextNode
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+
+
+static_dir_path = "./static"
+public_dir_path = "./public"
+
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    return None
+    print("Deleting public directory...")
+    if os.path.exists(public_dir_path):
+        shutil.rmtree(public_dir_path)
+    
+    print("Copying static files to public directory...")
+    copy_files_recursive(static_dir_path, public_dir_path)
 
 if __name__ == "__main__":
     main()
