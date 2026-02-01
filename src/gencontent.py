40eda2e3cf1db19c79d9450b0ee7d71a63ad5258
Kota117 <dakotah1375@gmail.com>
2026-02-01 09:59:01 -0700

    Can generate a single html from markdown

---
+++ src/gencontent.py
@@ -0,0 +1,32 @@
+import os
+from markdown_blocks import markdown_to_html_node
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f" * {from_path} {template_path} -> {dest_path}")
+    with open(from_path, 'r') as from_file:
+        markdown_content = from_file.read()
+
+    with open(template_path, 'r') as template_file:
+        template = template_file.read()
+
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
+
+    title = extract_title(markdown_content)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    with open(dest_path, 'w') as to_file:
+        to_file.write(template)
+
+
+def extract_title(markdown):
+    lines = markdown.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("no title found")
