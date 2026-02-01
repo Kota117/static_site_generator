72e8cbbc552cb9b6c10e61b2ab913d620ead59fb
Kota117 <dakotah1375@gmail.com>
2026-02-01 10:47:08 -0700

    Can generate html pages for all items in content

--- src/gencontent.py
+++ src/gencontent.py
@@ -2,6 +2,14 @@ import os
 from markdown_blocks import markdown_to_html_node
 
 
+def extract_title(markdown):
+    lines = markdown.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("no title found")
+
+
 def generate_page(from_path, template_path, dest_path):
     print(f" * {from_path} {template_path} -> {dest_path}")
     with open(from_path, 'r') as from_file:
@@ -24,9 +32,14 @@ def generate_page(from_path, template_path, dest_path):
         to_file.write(template)
 
 
-def extract_title(markdown):
-    lines = markdown.split("\n")
-    for line in lines:
-        if line.startswith("# "):
-            return line[2:]
-    raise ValueError("no title found")
+def generate_pages_recursive(content_dir_path, template_path, dest_dir_path):
+    for filename in os.listdir(content_dir_path):
+        content_path = os.path.join(content_dir_path, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(content_path):
+            generate_page(content_path, template_path, dest_path.replace(".md", ".html"))
+        else:
+            if not os.path.exists(dest_path):
+                os.mkdir(dest_path)
+            generate_pages_recursive(content_path, template_path, dest_path)
+
